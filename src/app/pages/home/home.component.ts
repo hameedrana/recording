66ff026a97d659179ceec663c78bac7ece8a0ccc
hameedrana <0hameedrana@gmail.com>
2022-11-29 01:09:16 -0800

    set values

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,49 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { StreamState } from 'src/app/interfaces/stream-state';
+import { AudioService } from 'src/app/services/audio.service';
+import { CloudService } from 'src/app/services/cloud.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-  state:any;
+  files: Array<any> = [];
+  state!: StreamState;
   currentFile: any = {};
-  constructor() { }
+
+
+  constructor(private audioService: AudioService, cloudService: CloudService) {
+    // get media files
+    cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
+  }
+
 
   ngOnInit(): void {
   }
-  files: Array<any> = [
-    { name: "First Song", artist: "Inder" },
-    { name: "Second Song", artist: "You" }
-  ];
-  
+
+  playStream(url:any) {
+    this.audioService.playStream(url).subscribe(events => {
+      // listening for fun here
+    });
+  }
+
+
+  openFile(file:any, index:any) {
+    this.currentFile = { index, file };
+    this.audioService.stop();
+    this.playStream(file.url);
+  }
+
+  pause() {
+    this.audioService.pause();
+  }
+
+
+  play() {
+    this.audioService.play();
+  }
+
+  stop() {
+    this.audioService.stop();
+  }
+
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  previous() {
+    const index = this.currentFile.index - 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
 
   isFirstPlaying() {
-    return false;
+    return this.currentFile.index === 0;
   }
+
   isLastPlaying() {
-    return true;
+    return this.currentFile.index === this.files.length - 1;
   }
 
-  openFile(file:any, i:any){
-  //  Console.log(file,i) 
-  }
-  onSliderChangeEnd(ev:any){
-
+  onSliderChangeEnd(change:any) {
+    this.audioService.seekTo(change.value);
   }
 
-  previous(){}
 
-
-  play(){}
-
-  pause(){}
-
-
-  next(){}
-
-
-
+  
 }
 
 
