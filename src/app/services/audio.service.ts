92d83feec54f723d9d08522815e1ecd2ea49104b
hameedrana <0hameedrana@gmail.com>
2022-11-29 00:23:56 -0800

    create audio service

---
+++ src/app/services/audio.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import * as moment from "moment";
+@Injectable({
+  providedIn: 'root'
+})
+export class AudioService {
+
+  private stop$ = new Subject();
+  private audioObj = new Audio();
+  audioEvents = [
+    "ended",
+    "error",
+    "play",
+    "playing",
+    "pause",
+    "timeupdate",
+    "canplay",
+    "loadedmetadata",
+    "loadstart"
+  ];
+  playStream(url:any) {
+    return this.streamObservable(url).pipe:any(takeUntil(this.stop$));
+  }
+  private streamObservable(url:any) {
+    new Observable(observer => {
+      // Play audio
+      this.audioObj.src = url;
+      this.audioObj.load();
+      this.audioObj.play();   
+
+      const handler = (event: Event) => {
+        observer.next(event);
+      };
+
+      this.addEvents(this.audioObj, this.audioEvents, handler);
+      return () => {
+        // Stop Playing
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0;
+        // remove event listeners
+        this.removeEvents(this.audioObj, this.audioEvents, handler);
+      };
+    });
+  }
+
+  private addEvents(obj:any, events:any, handler:any) {
+    events.forEach((event:any) => {
+      obj.addEventListener(event, handler);
+    });
+  }
+
+  private removeEvents(obj:any, events:any, handler:any) {
+    events.forEach((event:any) => {
+      obj.removeEventListener(event, handler);
+    });
+  }
+
+  
+
+  play() {
+    this.audioObj.play();
+  }
+
+  pause() {
+    this.audioObj.pause();
+  }
+
+  stop() {
+    this.stop$.next();
+  }
+
+  seekTo(seconds:any) {
+    this.audioObj.currentTime = seconds;
+  }
+
+  formatTime(time: number, format: string = "HH:mm:ss") {
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
+}
+
